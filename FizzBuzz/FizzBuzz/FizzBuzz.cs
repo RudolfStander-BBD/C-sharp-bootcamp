aa1567e48de1afe4e24c7d1e7c2e0a53ac8715c4
BBDNET\bbdnet1538 <rudolfs@bbd.co.za>
2017-04-04 15:29:53 +0200

    FizzBuzz lab

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzz
+    {
+        public static void printFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(getFizzBuzz(i) + " ");
+            }
+        }
+
+        public static string getFizzBuzz(int number)
+        {
+            if (number <= 0 || number > 70)
+            {
+                throw new InvalidOperationException("Modulo arithmetic below zero gets weird-ish");
+            }
+
+            bool fizzy = isFizz(number);
+            bool buzzy = isBuzz(number);
+
+            if (fizzy && buzzy)
+            {
+                return "FizzBuzz";
+            } else if (fizzy)
+            {
+                return "Fizz";
+            }
+            else if (buzzy)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
+
+        public static bool isFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        public static bool isBuzz(int number)
+        {
+            return number % 5 == 0;
+        }
+    }
+
+    public class FB
+    {
+        public static void Main()
+        {
+            FizzBuzz.printFizzBuzz();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/FizzBuzz_Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class FizzBuzz_Test
+    {
+        private string result;
+
+        [TestFixtureTearDown]
+        public void SetUp()
+        {
+            result = @"1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 
+                    16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 
+                    31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 
+                    46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 
+                    61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz";
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            result = null;
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(60, "FizzBuzz")]
+        [TestCase(64, "64")]
+        public void testNumber(int num, string aResult)
+        {
+            var returnedResult = FizzBuzz.getFizzBuzz(num);
+            Assert.That(returnedResult == aResult);
+        }
+
+        [Test]
+        [TestCase(200)]
+        [TestCase(-30)]
+        [TestCase(71)]
+        [TestCase(0)]
+        public void canThrowException(int? num)
+        {
+            var exceptionHandled = Assert.Throws<InvalidOperationException>(() => FizzBuzz.getFizzBuzz(Convert.ToInt32(num)));
+
+
+        }
+    }
+}
