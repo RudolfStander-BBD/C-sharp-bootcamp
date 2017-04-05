b7561dac74f96e3f74c6e6132c3b3a32139f981f
BBDNET\bbdnet1538 <rudolfs@bbd.co.za>
2017-04-05 11:46:12 +0200

    Added Swagger to CustomerWebApp

--- CustomerWebApi/CustomerWebApi/Startup.cs
+++ CustomerWebApi/CustomerWebApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using CustomerWebApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerWebApi
 {
@@ -36,6 +37,18 @@ namespace CustomerWebApi
             services.AddMvc();
 
             services.AddSingleton<ICustomerRepository, CustomerRepository>();
+
+            // Swagger stuffs
+            services.AddLogging();
+
+            // Add our repository type
+            services.AddSingleton<ICustomerRepository, CustomerRepository>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +57,17 @@ namespace CustomerWebApi
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
+            //swagger stuffs
+            app.UseMvcWithDefaultRoute();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
